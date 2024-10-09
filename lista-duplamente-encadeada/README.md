0ee7d1415f7fc9692e14ecec1b36f03b90eb8fc7
bastosanaa <bastosanaa11@gmail.com>
2024-10-09 14:45:15 -0300

    Finalizando implementação e documentação da minha lista duplamente encadeada

--- lista-duplamente-encadeada/README.md
+++ lista-duplamente-encadeada/README.md
@@ -1,10 +1,10 @@
+# Documentação - MinhaListaDuplamenteEncadeada
+
 # Classes
 
-- Elemento: classe que representa os Nodes que formarão a lista encadeada, contendo o seu valor e "apontando" para o seu sucessor
+- Elemento: classe que representa os Nodes que formarão a lista encadeada, contendo o seu valor, o seu antecessor e sucessor;
 
-- EmptyListError: exceção levantada quando uma operação é realizada em uma lista encadeada vazia;
-
-- MinhaListaDuplamenteEncadeada: classe que representa a implementação pedida para o trabalhinho 1
+- MinhaListaDuplamenteEncadeada: classe que representa a implementação pedida para o trabalhinho 1;
 
 # Funções:
 
@@ -32,10 +32,48 @@
 
 ## Funções de manipulação na lista
 
+A implementação da MinhaListaDuplamenteEncadeada possui algumas peculiaridades dadas pelas decisões do desenvolvedor, seguem estas:
+
+## Peculiaridades da Implementação
+
 1. acessarAtual - caso o cursor apontar para um elemento, retorna o valor do elemento apontado;
+
+Comportamento do cursor ao realizar as operações de lista:
+
+- Inserções = após todas as inserções o cursor se apontará para o elemento inserido
 
 2. inserirAntesDoAtual:
 
-3. inserirAposAtual -
+   - caso 1: cursor aponta para um elemento que possui antecessor (do meio da lista);
+   - caso 2: cursor aponta para o primeiro elemento da lista
+     no caso 2 a função não altera o elemento sucessor do elemento antecessor apontado pelo cursor e define o novo elemento como o inicio da lista
 
-4. inserirComoUltimo:
+3. inserirAposAtual:
+
+   - caso 1: cursor aponta para um elemento que possui sucessor (do meio da lista);
+   - caso 2: cursor aponta para o primeiro elemento da lista
+     no caso 2 a função não altera o elemento anteessor do elemento sucessor apontado pelo cursor e define o novo elemento como o fim da lista
+
+4. inserirPorPrimeiro;
+
+5. inserirPorUltimo;
+
+6. inserirNaPosicao:
+
+   - a localização da posição a ser inserida é feita por uma contagem dos elementos, partindo do incio da lista;
+   - caso a lista esteja vazia, o elemento é inserido como primeiro;
+   - caso a lista+1 ainda possua um número de elementos menor do que a da posição escolhida, o elemento é adicionado ao final da lista;
+
+7. excluirAtual: após a exclusão o cursor apontará para o elemento que precedia o elemento excluído
+
+8. excluirPrimeiro: após a exclusão o cursor apontará para o primeiro elemento
+
+9. excluirUltimo: após a exclusão o cursor apontará para o último elemento
+
+10. excluirElemento: exclui o elemento da lista que possui valor igual ao passado (considerando que não há valores repetidos na lista)
+
+11. exluirDaPosicao:
+    - a localização da posição a ter o elemento excluido é feita por uma contagem dos elementos, partindo do incio da lista;
+    - caso a lista esteja vazia, nada acontece;
+    - caso a posição ainda não esteja ocupada na lista, nada acontece
+    - o cursor aponta para o elemento que precede o elemento excluido, ou para o ultimo elemento caso nao haja exclusao
--- lista-duplamente-encadeada/lista-duplamente-encadeada.py
+++ lista-duplamente-encadeada/lista-duplamente-encadeada.py
@@ -49,14 +49,146 @@ class MinhaListaDuplamenteEncadeada:
         self.__cursor = self.__fim
 
     #Funções de Lista
+    def __inserirPrimeiro(self, elemento):
+        self.__inicio = elemento
+        self.__fim = elemento
+        self.__cursor = elemento
+    
+    # listar
     def acessarAtual(self) -> Elemento:
         if self.__cursor:
             return self.__cursor.getValor()
+        print('Lista Vazia')
+
+    def listaTodos(self):
+        print('-------')
+        if self.__cursor:
+            self.__irParaPrimeiro()
+            print(self.__cursor.getValor())
+            while self.__cursor != self.__fim:
+                self.__avancarKPosicoes(1)
+                print(self.__cursor.getValor())
         else:
-            print('Lista Vazia')
-    
+            print('lista vazia')
+        print('-------')
+
+    # inserir
     def inserirAntesAtual(self, valor) -> None:
         novo = Elemento(valor)
-        if self.__cursor == self.__inicio:
+        if self.__cursor:
+            novo.setAntecessor(self.__cursor.getAntecessor())
+            novo.setSucessor(self.__cursor)
+            if self.__cursor == self.__inicio:
+                self.__inicio = novo
+            else:
+                self.__cursor.getAntecessor().setSucessor(novo)
+            self.__cursor.setAntecessor(novo)
+            self.__cursor = novo
+        else:
+            self.__inserirPrimeiro(novo)
             
+    def inserirAposAtual(self, valor) -> None:
+        novo = Elemento(valor)
+        if self.__cursor:
+            novo.setAntecessor(self.__cursor)
+            novo.setSucessor(self.__cursor.getSucessor())
+            if self.__cursor == self.__fim:
+                self.__fim = novo
+            else:
+                self.__cursor.getSucessor().setAntecessor(novo)
+            self.__cursor.setSucessor(novo)
+            self.__cursor = novo
+        else:
+            self.__inserirPrimeiro(novo)
+
+    def inserirPorUltimo(self, valor) -> None:
+        novo = Elemento(valor)
+        if self.__fim:
+            novo.setAntecessor(self.__fim)
+            self.__fim.setSucessor(novo)
+        self.__fim = novo
+        self.__cursor = self.__fim
+    
+    def inserirPorPrimeiro(self, valor) -> None:
+        novo = Elemento(valor)
+        if self.__inicio:
+            novo.setSucessor(self.__inicio)
+            self.__inicio.setAntecessor(novo)
+        self.__inicio = novo
+        self.__cursor = self.__inicio
+
+    def inserirNaPosicao(self,k ,valor) -> None:
+        if self.__cursor:
+            self.__irParaPrimeiro()
+            self.__avancarKPosicoes(k-1)
+            self.inserirAposAtual(valor)
+        else:
+            novo = Elemento(valor)
+            self.__inserirPrimeiro(novo)
+    
+    # excluir
+    def excluirAtual(self) -> None:
+        if self.__cursor:
+            if self.__cursor == self.__fim:
+                return self.excluirUltimo()
+            if self.__cursor == self.__inicio:
+                return self.excluirPrimeiro()
+            antecessor = self.__cursor.getAntecessor()
+            sucessor = self.__cursor.getSucessor()
+            antecessor.setSucessor(sucessor)
+            sucessor.setAntecessor(antecessor)
+            self.__cursor = antecessor
+
+    def excluirPrimeiro(self) -> None:
+        if self.__inicio and self.__inicio.getSucessor():
+            self.__inicio = self.__inicio.getSucessor()
+            self.__inicio.setAntecessor(None)
+        else:
+            self.__inicio = None
+        self.__cursor = self.__inicio
+
+    def excluirUltimo(self) -> None:
+        if self.__fim and self.__fim.getAntecessor():
+            self.__fim = self.__fim.getAntecessor()
+            self.__fim.setSucessor(None)
+        else:
+            self.__fim = None
+        self.__cursor = self.__fim
+    
+    def excluirElemento(self, valor) -> None:
+        self.__irParaPrimeiro()
+        while self.__cursor.getValor() != valor:
+            if self.__cursor == self.__fim:
+                break
+            self.__avancarKPosicoes(1)
+        self.excluirAtual()
+    
+    def excluirDaPosicao(self, posicao) -> None:
+        self.__irParaPrimeiro()
+        self.__avancarKPosicoes(posicao)
+        self.excluirAtual()
+
+
+l1 = MinhaListaDuplamenteEncadeada()
+
+# realizando insercoes na lista
+l1.inserirNaPosicao(0, 20) #insere 20 como primeiro elemento
+l1.inserirAposAtual(40) #insere 40 apos 20
+l1.inserirAntesAtual(30) #insere 30 antes do 40
+l1.inserirPorPrimeiro(10) #insere 10 como primeiro, antes do 20
+l1.inserirPorUltimo(50) # insere 50 como ultimo, depois do 40
+l1.listaTodos()
+# saida esperada = 10, 20, 30, 40, 50
+
+#cursor no 50
+#realizando exclusoes na lista
+l1.excluirAtual() #exclui 50
+l1.excluirDaPosicao(1) #exclui 20
+l1.excluirElemento(30) #exclui 30
+l1.excluirPrimeiro() #exclui 10
+l1.excluirUltimo() #exclui 40
+l1.listaTodos()
+#saida esperada = lista vazia
+
+
 
